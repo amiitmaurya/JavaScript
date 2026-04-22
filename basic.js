1f048b3149f5284c7ba20092859082e08748b1ff
Amit  Maurya <115458433+amiitmaurya@users.noreply.github.com>
2026-04-22 19:36:20 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,14 @@
+const accountId =3110     // Constant which can'be change
+let accountEmail ="amitmaurya921@gmail.com" //
+var accountPassword="12345" //can be change
+accountCity="Jaipur"
+
+//accountId=2
+accountEmail="p@gmail.com"
+accountPassword="5685"
+accountCity="Delhi"
+ let accountState;
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+/* prefer not to use var for declear variable due to block  scope{} and function scope issue*/
+/* Semicolumn is not mandetory to use */
